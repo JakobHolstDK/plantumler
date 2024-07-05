8bc767299fc2d0c2c2a23f6c75702d39d97a7b7f
Jakob Holst <jakob.holst@dsv.com>
2024-07-05 13:55:05 +0200

    demo

--- uuencode2png.py
+++ uuencode2png.py
@@ -5,7 +5,7 @@ import base64
 import os
 import sys
 
-def uuencode2png(json_file):
+def uuencode2png(json_file, output_file='output.png'):
     """
     Convert a uuencoded image from a JSON file to a PNG file.
 
@@ -25,7 +25,7 @@ def uuencode2png(json_file):
         # Decode the image
         decoded_image = base64.b64decode(data['image'])
         # Write the image to a PNG file
-        with open('output.png', 'wb') as f:
+        with open(outout_file, 'wb') as f:
             f.write(decoded_image)
     except Exception as e:
         raise e
@@ -35,8 +35,12 @@ if __name__ == '__main__':
         print("Usage: python uuencode2png.py <json_file>")
         sys.exit(1)
     json_file = sys.argv[1]
-    uuencode2png(json_file)
+    outout_file = sys.argv[2]
+    uuencode2png(json_file, output_file)
 
-# Usage: python uuencode2png.py <json_file>
+# Usage: python uuencode2png.py <json_file> output.png
 # Example: python uuencode2png.py image.json
+
+# 2024-07-05 13:34:00 - Last updated by: Jakob Holst
+
 
